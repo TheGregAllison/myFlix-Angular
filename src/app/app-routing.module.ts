f87711f1b5e402e8d739c9c77afe2ecb68b34771
TheGregAllison <146021687+TheGregAllison@users.noreply.github.com>
2024-03-01 11:18:35 -0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component'; // Import the WelcomeComponent
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
